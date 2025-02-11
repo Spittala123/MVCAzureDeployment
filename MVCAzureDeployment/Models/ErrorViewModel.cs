e60aa42320a1daf03cb9dd12db212ffb1656f80e
Sravya Pittala <Sravya.Pittala@ibm.com>
2025-02-11 17:40:51 +0530

    Add project files.

---
+++ MVCAzureDeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCAzureDeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
